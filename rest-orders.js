55c6c67d8d1e82e44198145d4dcc4bde23281b56
Stephan Kristyn <kristyn@meshfields.de>
2019-08-09 00:53:50 +0200

    Added Test Concurrency in Npm for chainability of  all tests.

--- rest-orders.js
+++ rest-orders.js
@@ -31,13 +31,19 @@ request.post(options, (error, response, body) => {
 
     console.log(body);
 
-    assert.equal(body[0].length, 32, 'Must be an Array of Length 32');
-    assert.typeOf(body[0][0], 'Number', 'Must be a Number');
-    assert.typeOf(body[0][3], 'String', 'Must be a String');
-    assert.typeOf(body[0][6], 'Number', 'Must be a Number');
-    assert.typeOf(body[0][7], 'Number', 'Must be a Number');
-    assert.typeOf(body[0][8], 'String', 'Must be a String');
-    assert.typeOf(body[0][16], 'Number', 'Must be a Number');
+    if (body[0] !== undefined) {
+        assert.equal(body[0].length, 32, 'Must be an Array of Length 32');
+        assert.typeOf(body[0][0], 'Number', 'Must be a Number');
+        assert.typeOf(body[0][3], 'String', 'Must be a String');
+        assert.typeOf(body[0][6], 'Number', 'Must be a Number');
+        assert.typeOf(body[0][7], 'Number', 'Must be a Number');
+        assert.typeOf(body[0][8], 'String', 'Must be a String');
+        assert.typeOf(body[0][16], 'Number', 'Must be a Number');
 
-    console.log('\x1b[42m%s\x1b[0m', 'OK. Orders Array Length and Sample Types are valid.');
+        console.log('\x1b[42m%s\x1b[0m', 'OK. Orders Array Length and Sample Types are valid.');
+    } else {
+        console.log('\x1b[43m%s\x1b[0m', 'SKIPPED. You dont seem to have any orders.');
+    }
+
+
 });
